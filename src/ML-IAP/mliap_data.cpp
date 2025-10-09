cae27c15a02738011c6f1969132cc1e8a9e092bc
Richy <strunk876@gmail.com>
2025-10-09 15:27:38 +0200

    Add debug messages

--- src/ML-IAP/mliap_data.cpp
+++ src/ML-IAP/mliap_data.cpp
@@ -218,6 +218,11 @@ void MLIAPData::generate_neighdata(NeighList *list_in, int eflag_in, int vflag_i
         rij[ij][0] = delx;
         rij[ij][1] = dely;
         rij[ij][2] = delz;
+
+        // Print indices for the 'lmp_firstneigh' and 'firstneigh' arrays.
+        // Compare these against their respective allocated sizes.
+        printf("DEBUG: Accessing neighbors. ii = %d, ninside = %d, i = %d, jj = %d\n", ii, ninside, i, jj);
+
         lmp_firstneigh[ii][ninside] = firstneigh[i][jj];
         ij++;
         ninside++;
