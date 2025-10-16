b81515f96caabf1a09d01c1443c095e559bfbacc
Richy <strunk876@gmail.com>
2025-10-16 22:21:10 +0200

    Rename property

--- src/ML-IAP/mliap_unified_couple.pyx
+++ src/ML-IAP/mliap_unified_couple.pyx
@@ -154,7 +154,7 @@ cdef class MLIAPDataPy:
         return np.asarray(<double[:self.ntotal, :3]> &self.data.x[0][0])
 
     @property
-    def charge(self):
+    def charges(self):
         if self.data.q is NULL:
             return None
         return np.asarray(<double[:self.ntotal]> &self.data.q[0])
